663f74e4b535d285ec33fd940a757ef7d7d20faf
Mattia Robuschi Caprara <mattia.r.c@icloud.com>
2022-02-24 11:23:10 +0100

    Tutorial 6

--- tutorial_flutter/lib/main.dart
+++ tutorial_flutter/lib/main.dart
@@ -65,9 +65,8 @@ class _HomePageState extends State<HomePage> {
           Expanded(
               child:
                   PostList(this.posts)), //Expanded uses all the space possible
-          Expanded(
-              child: TextBoxWidget(
-                  this.newPost /*reference to a function, not executing*/))
+          TextBoxWidget(this.newPost /*reference to a function, not executing*/),
+          SizedBox(height: 30)//Custom spacer
         ]));
   }
 }
@@ -122,12 +121,39 @@ class PostList extends StatefulWidget {
 }
 
 class _PostListState extends State<PostList> {
+  
+  void like(Function callback) {
+    this.setState(() {
+      callback();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: this.widget.listItems.length,
       itemBuilder: (context, index) {
         var post = this.widget.listItems[index];
+        return Card(
+            child: Row(children: <Widget>[
+          Expanded(
+              child: ListTile(
+                  title: Text(post.body), subtitle: Text(post.author))),
+          Row(
+            children: <Widget>[
+              Container(
+                child:
+                    Text(post.likes.toString(), style: TextStyle(fontSize: 20)),
+                padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+              ),
+              IconButton(
+                icon: Icon(Icons.thumb_up_alt),
+                onPressed: () => this.like(post.likePost),
+                color: post.userLiked ? Colors.blue : Colors.black,//? = if true, : = else
+              )
+            ],
+          )
+        ]));
       },
     );
   }
